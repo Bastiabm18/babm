2972915a35bd42968d59f1e1252bb66ac34d6127
Bastiabm18 <andresbarriosmedina1@gmail.com>
2026-04-30 00:09:26 -0400

    babm a supabase v0.2

--- src/types/admin.ts
+++ src/types/admin.ts
@@ -18,3 +18,25 @@ export interface FaqFormState {
   message: string;
   faq?: Faq;
 }
+
+
+export interface PostImage {
+  url: string;
+  path: string;
+}
+
+export interface Post {
+  id: string;
+  titulo: string;
+  subtitulo: string;
+  descripcion: string;
+  contenido?: string; 
+  fecha: string; // Se guarda como string ISO desde el frontend
+  imagenes: PostImage[];
+}
+
+export interface FormState {
+  success: boolean;
+  message: string;
+  post?: Post; // Opcional porque al eliminar no devuelve el post
+}
